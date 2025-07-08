59e4da80068bb988a3d792919a1703253e535bce
mohamedhassanmn <hassanaesthete008@gmail.com>
2025-07-08 12:17:44 +0530

    update apiversion

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -3,6 +3,6 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
-  apiVersion: "2024-01-01",
+  apiVersion: "2025-07-08",
   useCdn: false,
 });
